1a0ec691eb6fe26666138f84fe416ab5d558e403
Elizabeth <ekkjohnson@gmail.com>
2022-04-20 16:37:46 -0500

    post function

--- Develop/db/server.js
+++ Develop/db/server.js
@@ -24,3 +24,19 @@ app.get('/api/notes', (req, res)=>{
     console.info(`${req.method} request received for adding note`);
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
+
+app.post('/api/notes', (req, res)=>{
+    console.log(`${req.method} request received to save a note`)
+    const { title, text } = req.body;
+    if (req.body) {
+        const newNote = {
+          title,
+          text,
+        };
+        readAndAppend(newNote, './db/db.json');
+        res.json('Note added');
+    } else{
+        res.error ('error in adding your note')
+    }
+
+});
