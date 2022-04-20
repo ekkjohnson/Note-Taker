635ff46a689aabdd4d062e0ad3e62f6c8c1d7eaa
Elizabeth <ekkjohnson@gmail.com>
2022-04-20 16:22:49 -0500

    server and file structure

---
+++ Develop/db/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+// const apiRoutes = require('./routes/apiRoutes');
+// const htmlRoutes = require('./routes/htmlRoutes');
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.listen(PORT, () => console.log(`Listening on PORT: PORT`));
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
