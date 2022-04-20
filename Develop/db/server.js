e2bcc7b3bf0461579c3f9f0782cb9404044b08ba
Elizabeth <ekkjohnson@gmail.com>
2022-04-20 16:30:45 -0500

    get functions

--- Develop/db/server.js
+++ Develop/db/server.js
@@ -5,6 +5,22 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
 app.use(express.static('public'));
 app.listen(PORT, () => console.log(`Listening on PORT: PORT`));
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res)=>{
+    res.sendFile(path.join(__dirname,'/public/notes.html'))
+});
+
+app.get('/api/notes', (req, res)=>{
+    console.info(`${req.method} request received for adding note`);
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
