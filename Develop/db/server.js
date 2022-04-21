a770f4b07dce6d21fbb904ee64f1ab4a4bd3c656
Elizabeth <ekkjohnson@gmail.com>
2022-04-21 17:52:35 -0500

    bug fixes

--- Develop/db/server.js
+++ Develop/db/server.js
@@ -5,12 +5,12 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
-
 app.use(express.static('public'));
+
+app.use("/", apiRoutes)
+app.use("/", noteRoutes)
 app.listen(PORT, () => console.log(`Listening on PORT: PORT`));
-app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 });
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const data = require('../db/db.json');
 const fs = require('fs')
 const exportData = (data) => {
-    fs.writeFileSync("./db/db.json", JSON.stringify(data))
+    fs.writeFileSync("../db/db.json", JSON.stringify(data))
 }
 router.get("/api/notes", (req,res) => {
     res.json(data)
