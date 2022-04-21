32f289eeac7c81423ced07874042b2f6d1f1ae00
Elizabeth <ekkjohnson@gmail.com>
2022-04-21 17:00:56 -0500

    html

--- Develop/db/server.js
+++ Develop/db/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 // const apiRoutes = require('./routes/apiRoutes');
 // const htmlRoutes = require('./routes/htmlRoutes');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', apiRoutes);
