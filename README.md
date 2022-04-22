3bfa02f8cb492b8c83fd34665e8c108054c637cf
Elizabeth <ekkjohnson@gmail.com>
2022-04-21 21:17:40 -0500

    read me

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Note-Taker
 Elizabeth's Note-Taker app
+![Screen Shot 2022-04-21 at 9 13 21 PM](https://user-images.githubusercontent.com/97856843/164582893-bd2fbf1c-3421-46be-a16b-170e5529d7ac.png)
+
+## Table of Contents:
+  [Contact](#contact)<br>
+  [Description](#description)<br>
+  [Installation](#installation)<br>
+  [Usage](#usage)<br>
+  [Guidelines](#guidelines)<br>
+  [Tests](#tests)<br>
+  [License](#license)<br>
+  
+# Contact
+ Elizabeth
+ ekkjohnson
+ ekkjohnson@gmail.com
+ # Description
+  lets you create and save notes, very handy to keep track of things you want to remember. 
+  
+  # License
+  ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)<br>
+  [The MIT license](https://opensource.org/licenses/MIT)
+  
+
+
--- server.js
+++ server.js
@@ -1,24 +1,18 @@
 const path = require('path')
 const fs = require('fs');
 const express = require('express');
-
-
-// const apiRoutes = require('./routes/apiRoutes');
-// const noteRoutes = require('./routes/noteRoute');
 const app = express();
 const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// app.use("/", apiRoutes)
-// app.use("/", noteRoutes)
 
-
+//function to get notes html
 app.get('/notes', (req, res)=>{
     res.sendFile(path.join(__dirname,'/public/notes.html'))
 });
-
+//gets api notes 
 app.get('/api/notes', (req, res)=>{
     const getNote = fs.readFileSync(path.join(__dirname, '/db/db.json'), "utf-8");
   const jsonparseNote = JSON.parse(getNote);
@@ -26,7 +20,7 @@ app.get('/api/notes', (req, res)=>{
     console.info(`${req.method} note additon request`);
  
 });
-
+//posts created note
 app.post('/api/notes', (req, res)=>{
     const saveNote = fs.readFileSync(path.join(__dirname, '/db/db.json'), "utf-8");
   const parseNote = JSON.parse(saveNote);
